246022a0fefffca5adbeb1b2acd0e902e8b2f0d4
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-06 01:32:27 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,69 @@
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |item, properties|
+      if !cart_hash.has_key?(item)
+        cart_hash[item] = {:count => 1}
+        cart_hash[item] = cart_hash[item].merge(properties)
+      else
+        cart_hash.has_key?(item)
+        cart_hash[item][:count] += 1
+      end
+    end
+  end
+  return cart_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon_hash|
+    if cart.has_key?(coupon_hash[:item]) && cart[coupon_hash[:item]][:count] >= coupon_hash[:num]
+      if cart.has_key?("#{coupon_hash[:item]} W/COUPON") && cart[coupon_hash[:item]][:count] >= coupon_hash[:num]
+        cart["#{coupon_hash[:item]} W/COUPON"][:count] += 1
+        cart[coupon_hash[:item]][:count] -= coupon_hash[:num]
+      else
+        cart["#{coupon_hash[:item]} W/COUPON"] = {:price => coupon_hash[:cost], :clearance => cart[coupon_hash[:item]][:clearance], :count => 1}
+        cart[coupon_hash[:item]][:count] -= coupon_hash[:num]
+      end
+    end
+    
+  end
+  return cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, data|
+    if data[:clearance]
+      data[:price] *= 0.8
+      data[:price] = data[:price].round(3)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  
+  total = 0
+  cart.each do |item, data|
+    total += data[:price]*data[:count]
+  end
+  if total > 100
+    total *= 0.9
+  end
+  return total
 end
+
+
+cart = [
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }}
+]
+
+coupons = [
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "AVOCADO", :num => 2, :cost => 5.00}
+    ]
